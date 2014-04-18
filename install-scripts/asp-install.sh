0e77cadb3b292a2f701ea9b5e65a0312f4bfbd6a
Charles Nguyen <ctn@umn.edu>
2014-04-18 10:28:34 -0500

    Built a commandline installer for systems w/o Docker

---
+++ install-scripts/asp-install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+echo 
+echo "Please specify a path to install to:"
+read tools
+
+mkdir -p $tools
+export	PATH=$tools/anaconda/bin:$tools/gdal/bin:$PATH:$tools/StereoPipeline-2.3.0-x86_64-Linux-GLIBC-2.5/bin
+export	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$tools/StereoPipeline-2.3.0-x86_64-Linux-GLIBC-2.5/lib
+
+# Install MiniConda Python distribution
+# This will build the base directory /tools for all following software
+
+cd $tools && \
+wget http://repo.continuum.io/miniconda/Miniconda-3.3.0-Linux-x86_64.sh && \
+sh Miniconda-3.3.0-Linux-x86_64.sh -b -p $tools/anaconda && \
+rm -f Miniconda*
+echo y | conda install numpy scipy
+
+# Install CFITSIO
+cd $tools && \
+wget ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio3360.tar.gz && \
+tar xvfz cfitsio3360.tar.gz && \
+cd cfitsio && \
+./configure --prefix=$tools/cfitsio --enable-sse2 --enable-ssse3 --enable-reentrant && \
+make -j && make install && \
+cd $tools && rm -rf cfitsio*
+
+# GEOS
+cd $tools && \
+wget http://download.osgeo.org/geos/geos-3.4.2.tar.bz2 && \
+tar xvfj geos-3.4.2.tar.bz2 && \
+cd geos-3.4.2 && \
+export SWIG_FEATURES="-I/usr/share/swig/1.3.40/python -I/usr/share/swig/1.3.40" && \
+./configure --prefix=$tools/geos --enable-python && \
+make -j && make install && \
+cd $tools  && rm -rf geos*
+
+# PROJ
+cd $tools && \
+wget http://download.osgeo.org/proj/proj-4.8.0.tar.gz && \
+tar xvfz proj-4.8.0.tar.gz && \
+cd proj-4.8.0 && \
+./configure --prefix=$tools/proj --with-jni=no && \
+make -j && make install && \
+cd $tools && rm -rf proj*
+
+# OPENJPEG
+cd $tools && \
+wget https://openjpeg.googlecode.com/files/openjpeg-2.0.0-Linux-i386.tar.gz && \
+tar xvfz openjpeg-2.0.0-Linux-i386.tar.gz -C $tools  && \
+rm -rf openjpeg*
+
+# GDAL
+# Parallel make will fail due to race conditions. Do not use -j
+cd $tools && \
+wget http://download.osgeo.org/gdal/1.11.0/gdal-1.11.0beta1.tar.gz && \
+tar xvfz gdal-1.11.0beta1.tar.gz && \
+cd gdal-1.11.0beta1 && \
+./configure --prefix=$tools/gdal --with-geos=/tools/geos/bin/geos-config --with-cfitsio=/tools/cfitsio \
+--with-python --with-openjpeg=/tools/openjpeg-2.0.0-Linux-i386 --with-sqlite3=no && \
+make && make install && \
+cd $tools && rm -rf gdal*
+
+export	GDAL_DATA=$tools/gdal/share/gdal
+
+# Install Ames Stereo Pipeline
+cd $tools && \
+wget http://byss.ndc.nasa.gov/stereopipeline/binaries/StereoPipeline-2.3.0-x86_64-Linux-GLIBC-2.5.tar.bz2 && \
+tar xvfj StereoPipeline-2.3.0-x86_64-Linux-GLIBC-2.5.tar.bz2 -C $tools && \
+rm StereoPipeline-2.3.0-x86_64-Linux-GLIBC-2.5.tar.bz2
+
+
+
+echo "export	PATH=$PATH:\$PATH" >> ~/init-asp.sh
+echo "export	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:\$LD_LIBRARY_PATH" >> ~/init-asp.sh
