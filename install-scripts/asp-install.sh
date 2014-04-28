fde308d1124d90e7d95bb8fbbb0a340a000e14fd
Charles Nguyen <ctn@umn.edu>
2014-04-28 14:26:15 -0500

    Updated all install scripts to use installers hosted on github

--- install-scripts/asp-install.sh
+++ install-scripts/asp-install.sh
@@ -12,14 +12,14 @@ export	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$tools/gdal/lib:$tools/openjpeg-2/lib:$t
 # This will build the base directory /tools for all following software
 
 cd $tools && \
-wget http://repo.continuum.io/miniconda/Miniconda-3.3.0-Linux-x86_64.sh && \
+wget https://github.com/PolarGeospatialCenter/asp/raw/master/originals/Miniconda/Miniconda-3.3.0-Linux-x86_64.sh && \
 sh Miniconda-3.3.0-Linux-x86_64.sh -b -p $tools/anaconda && \
 rm -f Miniconda*
 echo y | conda install numpy scipy
 
 # Install CFITSIO
 cd $tools && \
-wget ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio3360.tar.gz && \
+wget https://github.com/PolarGeospatialCenter/asp/raw/master/originals/cfitsio/cfitsio3360.tar.gz && \
 tar xvfz cfitsio3360.tar.gz && \
 cd cfitsio && \
 ./configure --prefix=$tools/cfitsio --enable-sse2 --enable-ssse3 --enable-reentrant && \
@@ -28,7 +28,7 @@ make -j && make install
 # GEOS
 export	SWIG_FEATURES="-I/usr/share/swig/1.3.40/python -I/usr/share/swig/1.3.40"
 cd $tools && \
-wget http://download.osgeo.org/geos/geos-3.4.2.tar.bz2 && \
+wget https://github.com/PolarGeospatialCenter/asp/raw/master/originals/geos/geos-3.4.2.tar.bz2 && \
 tar xvfj geos-3.4.2.tar.bz2 && \
 cd geos-3.4.2 && \
 ./configure --prefix=$tools/geos && \
@@ -36,7 +36,7 @@ make -j && make install
 
 # PROJ
 cd $tools && \
-wget http://download.osgeo.org/proj/proj-4.8.0.tar.gz && \
+wget https://github.com/PolarGeospatialCenter/asp/raw/master/originals/proj/proj-4.8.0.tar.gz && \
 tar xvfz proj-4.8.0.tar.gz && \
 cd proj-4.8.0 && \
 ./configure --prefix=$tools/proj --with-jni=no && \
@@ -45,7 +45,7 @@ make -j && make install
 # OPENJPEG
 # Change to cmake or cmake28 depending on what is installed
 cd $tools && \
-wget https://openjpeg.googlecode.com/files/openjpeg-2.0.0.tar.gz && \
+wget https://github.com/PolarGeospatialCenter/asp/raw/master/originals/openjpeg/openjpeg-2.0.0.tar.gz && \
 tar xvfz openjpeg-2.0.0.tar.gz && \
 cd openjpeg-2.0.0 && \
 cmake -DCMAKE_INSTALL_PREFIX=$tools/openjpeg-2 && \
@@ -56,9 +56,9 @@ make install
 # Parallel make will fail due to race conditions. Do not use -j
 export	SWIG_FEATURES="-I/usr/share/swig/1.3.40/python -I/usr/share/swig/1.3.40"
 cd $tools && \
-wget http://download.osgeo.org/gdal/1.11.0/gdal-1.11.0beta1.tar.gz && \
-tar xvfz gdal-1.11.0beta1.tar.gz && \
-cd gdal-1.11.0beta1 && \
+wget https://github.com/PolarGeospatialCenter/asp/raw/master/originals/gdal/gdal-1.11.0.tar.gz && \
+tar xvfz gdal-1.11.0.tar.gz && \
+cd gdal-1.11.0 && \
 ./configure --prefix=$tools/gdal --with-geos=$tools/geos/bin/geos-config --with-cfitsio=$tools/cfitsio \
 --with-python --with-openjpeg=$tools/openjpeg-2 --with-sqlite3=no && \
 make && make install && \
